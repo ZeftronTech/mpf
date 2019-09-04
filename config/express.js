a106a8d0f6d094836534bd66b76c0fa8883b2968
azizahtas <azizahtas@gmail.com>
2019-09-04 17:41:36 +0530

    Added CORS Fix

--- config/express.js
+++ config/express.js
@@ -83,6 +83,14 @@ module.exports = function(app, passport) {
     })
   );
 
+  app.use(function(req, res, next) {
+    //res.header("Access-Control-Allow-Origin", "http://localhost:"+config.client);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,HEAD");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
   // cookieParser should be above session
   app.use(cookieParser());
   app.use(
